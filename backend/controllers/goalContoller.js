e1b9e94584879b9c6a998408d1032b6179a70cbe
Andcraftt <andcraftt@gmail.com>
2025-02-05 17:12:49 +0100

    goalController update

--- backend/controllers/goalContoller.js
+++ backend/controllers/goalContoller.js
@@ -7,9 +7,8 @@ const User = require('../models/userModel')
 // @route   GET /api/goals
 // @access  Public
 const getGoals = asyncHandler(async (req,res) =>{
-    const goals = await Goal.find()
-
-    res.status(200).json(goals)
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 })
 
 // @desc    Set goals
