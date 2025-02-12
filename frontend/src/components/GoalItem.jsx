50354bac01a8ff0665c85cab426b237083e02e64
theMarcus145 <marcogilvalero@gmail.com>
2025-02-12 17:12:08 +0100

    Cambio para la X que solo salga si el ID es igual prueba 1

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,16 +1,24 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { deleteGoal } from '../features/goals/goalSlice'
 
 function GoalItem({ goal }) {
+  const { user } = useSelector((state) => state.auth) // Accedemos al usuario autenticado desde el estado global
   const dispatch = useDispatch()
+
+  // Comparamos el ID del usuario autenticado con el ID del creador del goal
+  const isOwner = user && goal.userId === user._id
 
   return (
     <div className='goal'>
       <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
       <h2>{goal.text}</h2>
-      <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
-        X
-      </button>
+
+      {/* Solo mostramos el botón X si el usuario es el propietario del goal */}
+      {isOwner && (
+        <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
+          X
+        </button>
+      )}
     </div>
   )
 }
