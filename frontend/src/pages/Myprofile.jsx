39070f00936d895cbe5169e611fac72463290008
theMarcus145 <marcogilvalero@gmail.com>
2025-02-17 16:38:05 +0100

    fix indentacion

--- frontend/src/pages/Myprofile.jsx
+++ frontend/src/pages/Myprofile.jsx
@@ -6,55 +6,54 @@ import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
 
-function Myprofile(){
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+function Myprofile(){const navigate = useNavigate()
+  const dispatch = useDispatch()
   
-    const { user } = useSelector((state) => state.auth)
-    const { goals, isLoading, isError, message } = useSelector(
-      (state) => state.goals
-    )
+  const { user } = useSelector((state) => state.auth)
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  )
   
-    useEffect(() => {
-      if (isError) {
-        console.log(message)
-      }
-  
-      if (!user) {
-         navigate('/login')
-      }else {
-        dispatch(getGoals())
-      }
-      return () => {
-        dispatch(reset())
-      }
-    }, [user, navigate, isError, message, dispatch])
-  
-    if (isLoading) {
-      return <Spinner />
+  useEffect(() => {
+    if (isError) {
+      console.log(message)
     }
 
-    return (
-        <>
-          <section className='heading'>
-            <h1>Welcome {user && user.name}</h1>
-            <p>Goals Dashboard</p>
-          </section>
-    
-          {user && <GoalForm />}
-    
-          <section className='content'>
-            {goals.length > 0 ? (
-              <div className='goals'>
-                {goals.map((goal) => (
-                  <GoalItem key={goal._id} goal={goal} />
-                ))}
-              </div>
-            ) : (
-              <h3>You have not set any goals</h3>
-            )}
-          </section>
-        </>
-      )
+    if (!user) {
+       navigate('/login')
+    }else {
+      dispatch(getGoals())
+     }
+     return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, isError, message, dispatch])
+  
+  if (isLoading) {
+    return <Spinner />
+  }
+
+   return (
+      <>
+        <section className='heading'>
+          <h1>Welcome {user && user.name}</h1>
+          <p>Goals Dashboard</p>
+        </section>
+  
+        {user && <GoalForm />}
+  
+        <section className='content'>
+          {goals.length > 0 ? (
+            <div className='goals'>
+              {goals.map((goal) => (
+                <GoalItem key={goal._id} goal={goal} />
+              ))}
+            </div>
+          ) : (
+            <h3>You have not set any goals</h3>
+          )}
+        </section>
+       </>
+    )
 }
 export default Myprofile
