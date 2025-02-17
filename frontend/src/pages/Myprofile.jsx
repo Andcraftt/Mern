8dd6622ef83190613571fb5f96b09dde208d9f25
Andcraftt <andcraftt@gmail.com>
2025-02-17 16:31:39 +0100

    Changes myprofile

--- frontend/src/pages/Myprofile.jsx
+++ frontend/src/pages/Myprofile.jsx
@@ -33,5 +33,28 @@ function Myprofile(){
     if (isLoading) {
       return <Spinner />
     }
+
+    return (
+        <>
+          <section className='heading'>
+            <h1>Welcome {user && user.name}</h1>
+            <p>Goals Dashboard</p>
+          </section>
+    
+          {user && <GoalForm />}
+    
+          <section className='content'>
+            {goals.length > 0 ? (
+              <div className='goals'>
+                {goals.map((goal) => (
+                  <GoalItem key={goal._id} goal={goal} />
+                ))}
+              </div>
+            ) : (
+              <h3>You have not set any goals</h3>
+            )}
+          </section>
+        </>
+      )
 }
 export default Myprofile
