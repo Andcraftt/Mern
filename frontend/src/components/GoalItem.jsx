8f0b3962b59a40373cb90b0ebb6792b00b4487eb
Andcraftt <andcraftt@gmail.com>
2025-02-24 17:11:01 +0100

    Erase date from goals

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -10,7 +10,6 @@ function GoalItem({ goal }) {
 
   return (
     <div className='goal'>
-      <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
       <h2>{goal.text}</h2>
 
       {/* Si hay una URL de imagen, la mostramos */}
