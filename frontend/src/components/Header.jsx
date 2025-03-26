8581d2dc4fca6e793cc3cfb52da5897ad84325c4
theMarcus145 <marcogilvalero@gmail.com>
2025-03-26 17:03:32 +0100

    casita

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <IoHome /><Link to='/'></Link>
+        <Link to='/'><IoHome class="casita"/></Link>
       </div>
       <ul>
         {user ? (
