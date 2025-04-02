0b609c20c4cf8b259968ef77549c664bb9e6e13e
Andcraftt <andcraftt@gmail.com>
2025-04-02 16:13:34 +0200

    adada

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <Link to='/'><IoHome class="casita"/></Link>
+        <Link to='/'><IoHome id="casita"/></Link>
       </div>
       <ul>
         {user ? (
